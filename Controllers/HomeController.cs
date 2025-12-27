61beae65c167cd9e6fce50e1c55357a130bae080
MBaarrunbinjamal <mbaarrun2008@gmail.com~>
2025-12-27 15:00:48 +0500

    start

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using E_project2025.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_project2025.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
